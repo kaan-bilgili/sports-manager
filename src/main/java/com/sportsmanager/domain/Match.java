403a5bf5d8a22d8e5320374390986a3f07010595
Fatma Nur Erdoğan <fenurerdogan@gmail.com>
2026-04-08 21:36:42 +0300

    match
    Abstract base class for match simulation

--- src/main/java/com/sportsmanager/domain/Match.java
+++ src/main/java/com/sportsmanager/domain/Match.java
@@ -1,5 +1,47 @@
 package com.sportsmanager.domain;
 
-public class Match {
+public abstract class Match {
 
+    protected Team homeTeam;
+    protected Team awayTeam;
+    protected int homeScore;
+    protected int awayScore;
+    protected boolean played;
+
+    public Match(Team homeTeam, Team awayTeam) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeScore = 0;
+        this.awayScore = 0;
+        this.played = false;
+    }
+
+    public abstract void simulate();
+
+    public Team getHomeTeam() { return homeTeam; }
+    public Team getAwayTeam() { return awayTeam; }
+    public int getHomeScore() { return homeScore; }
+    public int getAwayScore() { return awayScore; }
+    public boolean isPlayed() { return played; }
+
+    public Team getWinner() {
+        if (!played) return null;
+        if (homeScore > awayScore) return homeTeam;
+        if (awayScore > homeScore) return awayTeam;
+        return null;
+    }
+
+    public boolean isDraw() {
+        return played && homeScore == awayScore;
+    }
+
+    @Override
+    public String toString() {
+        if (!played) {
+            return homeTeam.getName() + " vs "
+                    + awayTeam.getName() + " (not played)";
+        }
+        return homeTeam.getName() + " " + homeScore
+                + " - " + awayScore + " " + awayTeam.getName();
+    }
 }
