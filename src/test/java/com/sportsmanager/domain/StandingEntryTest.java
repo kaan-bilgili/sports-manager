8ad98cab6a8a7dccb264a8abc305b7b3b613dfa6
Karya21-pabu <tanrikulukarya@gmail.com>
2026-04-09 16:29:19 +0300

    test for StandingEntry added

--- src/test/java/com/sportsmanager/domain/StandingEntryTest.java
+++ src/test/java/com/sportsmanager/domain/StandingEntryTest.java
@@ -1,5 +1,26 @@
 package com.sportsmanager.domain;
 
-public class StandingEntryTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+class StandingEntryTest {
+
+    static class TestTeam extends Team {
+        public TestTeam(String name) { super(name); }
+    }
+
+    @Test
+    // Testtt
+    void testPointCalculation() {
+        TestTeam team = new TestTeam("Team A");
+        StandingEntry entry = new StandingEntry(team);
+
+        entry.recordWin(2, 0);
+        entry.recordDraw(1, 1);
+        entry.recordLoss(0, 3);
+
+        assertEquals(4, entry.getPoints());
+        assertEquals(3, entry.getMatchesPlayed());
+        assertEquals(0, entry.getGoalDifference());
+    }
 }
