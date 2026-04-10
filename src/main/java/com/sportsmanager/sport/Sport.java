42eb7411a89c03182e5faca8cb18280b6de18771
Karya21-pabu <tanrikulukarya@gmail.com>
2026-04-10 18:29:11 +0300

    Sport class added

--- src/main/java/com/sportsmanager/sport/Sport.java
+++ src/main/java/com/sportsmanager/sport/Sport.java
@@ -1,5 +1,14 @@
 package com.sportsmanager.sport;
 
-public class Sport {
+import com.sportsmanager.domain.Match;
+import com.sportsmanager.domain.Player;
+import com.sportsmanager.domain.Team;
 
+public interface Sport {
+    String getSportName();
+    Team createTeam(String name);
+    Player createPlayer(String name, int age, int skillLevel);
+    Match createMatch(Team homeTeam, Team awayTeam);
+    int getSquadSize();
+    int getSubstituteCount();
 }
