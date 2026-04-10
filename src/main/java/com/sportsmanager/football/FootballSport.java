37d1491ad6e76bf4d9eae94c71e7777b2106fa46
Fatma Nur Erdoğan <fenurerdogan@gmail.com>
2026-04-11 00:41:50 +0300

    creates teams players and matches

--- src/main/java/com/sportsmanager/football/FootballSport.java
+++ src/main/java/com/sportsmanager/football/FootballSport.java
@@ -1,5 +1,45 @@
 package com.sportsmanager.football;
 
-public class FootballSport {
+import com.sportsmanager.domain.Match;
+import com.sportsmanager.domain.Player;
+import com.sportsmanager.domain.Team;
+import com.sportsmanager.sport.Sport;
 
+import java.util.Random;
+
+public class FootballSport implements Sport {
+
+    private static final String[] POSITIONS = {"GOALKEEPER", "DEFENDER",
+            "MIDFIELDER", "FORWARD"};
+    private static final Random random = new Random();
+
+    @Override
+    public String getSportName() {
+        return "Football";
+    }
+
+    @Override
+    public Team createTeam(String name) {
+        return new FootballTeam(name);
+    }
+
+    @Override
+    public Player createPlayer(String name, int age, int skillLevel) {
+        return new FootballPlayer(name, age, skillLevel, FootballPlayer.Position.FORWARD);
+    }
+
+    @Override
+    public Match createMatch(Team homeTeam, Team awayTeam) {
+        return new FootballMatch(homeTeam, awayTeam);
+    }
+
+    @Override
+    public int getSquadSize() {
+        return 11;
+    }
+
+    @Override
+    public int getSubstituteCount() {
+        return 3;
+    }
 }
