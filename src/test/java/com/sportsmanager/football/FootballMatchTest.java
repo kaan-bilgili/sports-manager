514b284af32e2b551fbe609e12bf67553a2607b3
Fatma Nur Erdoğan <fenurerdogan@gmail.com>
2026-04-11 01:29:22 +0300

    add match simulation status test

--- src/test/java/com/sportsmanager/football/FootballMatchTest.java
+++ src/test/java/com/sportsmanager/football/FootballMatchTest.java
@@ -1,5 +1,26 @@
 package com.sportsmanager.football;
 
-public class FootballMatchTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+class FootballMatchTest {
+    static FootballTeam createTeamWithPlayers(String name, int skillLevel) {
+        FootballTeam team = new FootballTeam(name);
+        for (int i = 0; i < 11; i++) {
+            team.addPlayer(new FootballPlayer(
+                    "Player" + i, 25, skillLevel,
+                    FootballPlayer.Position.MIDFIELDER));
+        }
+        return team;
+    }
+
+    @Test
+    void testMatchIsPlayedAfterSimulation() {
+        FootballTeam home = createTeamWithPlayers("Home FC", 75);
+        FootballTeam away = createTeamWithPlayers("Away FC", 70);
+        FootballMatch match = new FootballMatch(home, away);
+        assertFalse(match.isPlayed());
+        match.simulate();
+        assertTrue(match.isPlayed());
+    }
 }
